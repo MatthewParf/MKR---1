b7738b2e7e67ddb96cb1a8d9ff7a5033478147b4
MatthewParf <mathew.parfenuk@knu.ua>
2023-10-17 13:18:31 +0300

    Matrix

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 n = int(input("Введіть розмірність матриці (NxN). (Наприклад: 2): "))
+t = input("Ви саммі введете матрицю, чи її створити програмі? (+ - самі, - - програма)")
 
-matrix = []
-for i in range(n):
-    row = input(f"Введіть {n} цілих чисел для {i+1}-го рядку, розділені пробілами. (Наприклад: 4 6): ").split()
-    row = [int(x) for x in row]
-    matrix.append(row)
+if t == "-":
+
+    import random
+
+    matrix = [[random.randint(1, 100) for j in range(n)] for i in range(n)]
+
+if t == "+":
+    matrix = []
+    for i in range(n):
+        row = input(f"Введіть {n} цілих чисел для {i+1}-го рядку, розділені пробілами. (Наприклад: 4 6): ").split()
+        row = [int(x) for x in row]
+        matrix.append(row)
 
 def average(row):
     return sum(row) / len(row)
